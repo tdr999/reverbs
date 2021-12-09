1787dec3af3a3d87c6f1eb58da886004d4974234
George-Vlad ENACHE (100852) <george.enache0205@stud.etti.upb.ro>
2021-12-09 12:29:13 +0200

    New Commit

--- reverberatingDelayIntrinsic.c
+++ reverberatingDelayIntrinsic.c
@@ -39,9 +39,9 @@ short reverberating_delay(short x, Word16 dry, Word16 wet, Word16 g, short delay
 {
     //observatie: tudor, nu uita sa scalezi cu formula aia cu s din platforma
 	delay_samples = 44.1 * delay_ms;
-	if (indiceBuffer >= delay_samples || flagFirstPass == 1) //daca s a umplut bufferu
+	if (indiceBuffer >= delay_samples || flagFirstPass == 1) //daca s a umplut bufferul
 	{
-		flagFirstPass = 1; //ca sa intre in ifu asta de acuma nainte
+		flagFirstPass = 1; //ca sa intre in ifu asta de acuma inainte
 		indiceBuffer %= delay_samples; //ca sa nu depaseasca short; nu afectaza restu de cod
 		//scoatem primul esantion din coada
 		short popat = dequeue();
@@ -59,24 +59,45 @@ short reverberating_delay(short x, Word16 dry, Word16 wet, Word16 g, short delay
 	}
 }
 
+Word16 AllpassFilter(Word16 x, Word16 g, Word16 delay_ms){
+	delay_samples = 44.1 * delay_ms;
+	
+	if(indiceBuffer>= delay_samples || flagFirstPass == 1){
+		flagFirstPass = 1;
+		indiceBuffer %= delay_samples;
+		short popat = dequeue();
+		
+		append(L_add(x, mult(popat,g)));
+		return L_add(mult(x,(-g)),popat);
+	}
+	else{
+		append(x);
+		return mult(x,(-g));
+	}
+}
+
 
 
 int main()
 {
 	FILE *input = fopen("intrare.dat", "r+b");
 	FILE *output = fopen("iesireIntrinsic.dat", "w+b"); //experimentam cu coada
-	short x, sex;
+	FILE *outputAll = fopen("iesireAllpass.dat", "w+b");
+	Word16 x, retval;
 	
 	while(fscanf(input, "%hd", &x) != EOF)
 	{
-        sex =  (short)(reverberating_delay(x, WORD16(0.6), WORD16(0.7), WORD16(0.8), 80)); //de scos mizeria asta
-		fprintf(output, "%d ", sex);
-
+        retval = AllpassFilter(x, WORD16(0.6), 80); 
+		fprintf(outputAll, "%d ", retval);
+		retval =  reverberating_delay(x, WORD16(0.6), WORD16(0.7), WORD16(0.8), 80); 
+		fprintf(output, "%d ", retval);
 	}
+	
 
 	
 	fclose(input);
 	fclose(output);
+	fclose(outputAll);
 	return 0;
 	
 }
